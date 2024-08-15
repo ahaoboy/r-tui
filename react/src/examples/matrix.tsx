0ed6c283519c505a743f262fb2273026461c15aa
ahaoboy <504595380@qq.com>
2024-08-15 18:53:18 +0800

    react: add matrix example

---
+++ react/src/examples/matrix.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Box } from '../'
+import { getTerminalShape } from '@r-tui/terminal'
+
+const CHARS = "ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｦｲｸｺｿﾁﾄﾉﾌﾔﾖﾙﾚﾛﾝ012345789Z:.\"=*+-<>¦╌ç";
+
+
+function randomInt(n: number) {
+  return (Math.random() * n) | 0
+}
+
+function choice(s: string) {
+  return s[randomInt(s.length)]
+}
+
+const LIVE_COLOR = [
+  "#808080",
+  "#708070",
+  "#608060",
+  "#508050",
+  "#408040",
+  "#308030",
+  "#208020",
+  "#108010",
+  "#008000",
+  "#00FF00",
+]
+
+export function Column({ height, id }: { id: number, height: number }) {
+  const [y, setY] = useState(randomInt(height))
+  const update = useRef<() => void>()
+  const handle = useRef<number>(0)
+  const [history, setHistory] = useState<{
+    char: string,
+    live: number
+  }[]>([])
+
+  function getColor(i: number) {
+    return LIVE_COLOR[history[i]?.live || 0]
+  }
+  function getText(i: number) {
+    return history[i]?.char || " "
+  }
+
+  update.current = () => {
+    for (const i of history) {
+      if (i) {
+        i.live = Math.max(i.live - 1, 0)
+      }
+    }
+    const c = choice(CHARS)
+
+    let newY = (y + 1) % height
+    if (Math.random() < 0.01) {
+      newY = 0
+    }
+    setY(newY)
+    history[newY] = { char: c, live: LIVE_COLOR.length - 1 }
+    setHistory([...history])
+  }
+
+  useEffect(() => {
+    const speed = (randomInt(5) + 1) * 50
+    handle.current = +setInterval(() => {
+      update.current?.()
+    }, speed)
+  }, [])
+
+  return <Box
+    width={1}
+    height={height}
+    display='flex'
+    flexDirection='column'
+  >
+    {Array(height).fill(0).map((_, i) => <Box
+      backgroundColor='black'
+      color={getColor(i)}
+      key={i} text={getText(i)} />)}
+  </Box>
+}
+
+export function Matrix() {
+  const { width, height } = getTerminalShape()
+  return Array(width).fill(0).map((_, i) => <Column key={i} height={height} id={i} />)
+}
+
+export default Matrix
--- react/src/examples/rgb.tsx
+++ react/src/examples/rgb.tsx
@@ -1,15 +1,23 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Box } from "../"
 import { getTerminalShape } from "@r-tui/terminal"
 
 export default function App() {
   const { width, height } = getTerminalShape()
+  const [blue, setBlue] = useState(0)
+
+  useEffect(() => {
+    setInterval(() => {
+      setBlue(c => (c + 5) % 255)
+    }, 100)
+  }, [])
+
   return new Array(width * height).fill(0).map((_, i) => {
     const x = (i / width) | 0
     const y = i % width
     const r = (((x / height) * 255) | 0).toString(16).padStart(2, '0')
     const g = (((y / width) * 255) | 0).toString(16).padStart(2, '0')
-    const b = 'FF'
+    const b = blue.toString(16).padStart(2, '0')
     const c = `#${r}${g}${b}`
     return <Box id={i} key={i} color={c} text="█" />
   })
