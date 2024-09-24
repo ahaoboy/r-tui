5a2a8b1a4169dffcf898dd068867045a2bb4e97d
ahaoboy <504595380@qq.com>
2024-09-24 22:58:03 +0800

    terminal: add defaultShape

--- terminal/src/index.ts
+++ terminal/src/index.ts
@@ -1,5 +1,8 @@
 import { Shape } from "@r-tui/share"
-export function getTerminalShape(): Shape {
+export function getTerminalShape(defaultShape = { width: 256, height: 128 }): Shape {
+  if (typeof process === 'undefined' || process.stdout?.columns === undefined) {
+    return defaultShape
+  }
   const width = process.stdout.columns || 0
   const height = process.stdout.rows || 0
   return { width, height }
